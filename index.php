fb6b96444777dc67074bfb5150c10cfb3329d970
Fernando J Pando <fernando.pando@stelligent.com>
2015-12-27 18:02:18 -0500

    kill hitcounter

--- index.php
+++ index.php
@@ -17,44 +17,14 @@
 </script>
 </head>
 
-
 <body bgcolor=9933CC text=pink vlink=blue>
 <center><font face=Helvetica><p><br><p><b><h1>
 &nbsp; 
 </h1></b><p><br><p>
 <a href=http://www.damnswank.com/wordpress>
 <img src=http://www.damnswank.com/damnswank.gif>
-<p>&copy; 2001-2015 DAMNSWANK<br><p></a>
+<p>&copy; 2001-2016 DAMNSWANK<br><p></a>
 
-<?php
-        $COUNT_FILE = "counter.txt";
-        $IMG_DIR_URL = "http://www.damnswank.com/digits/";
-        $NB_DIGITS = 8;
-        if (file_exists($COUNT_FILE)) {
-                $fp = fopen("$COUNT_FILE", "r+");
-                flock($fp, 1);
-                $count = fgets($fp, 4096);
-                $count += 1; 
-                fseek($fp,0);
-                fputs($fp, $count);
-                flock($fp, 3);
-                fclose($fp);
-        }
-        else {
-                echo "Can't find file, check '\$file' var...<BR>";
-                exit;
-        }
-        chop($count);
-        $nb_digits = max(strlen($count), $NB_DIGITS);
-        $count = substr("0000000000".$count, -$nb_digits);
-        $digits = preg_split("//", $count);
-        for($i = 0; $i <= $nb_digits; $i++) {
-                if ($digits[$i] != "") {
-                        $html_result .=  "<IMG SRC=\"https://s3.amazonaws.com/damnswank/counter/[$i].gif\">";
-                }
-        } 
-        echo $html_result; 
-?>
 </body>
 </html>
 
