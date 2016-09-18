5ae54f2def20635db873e8b6b621164c13186033
Fernando J Pando <fernando.pando@stelligent.com>
2016-09-18 15:40:03 -0400

    docker-go

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM centos:7
+
+MAINTAINER "Fernando J Pando" <nando@hex7.com>
+
+RUN yum -y install git
+
+RUN git clone https://github.com/nand0p/damnswank.git
+
+RUN curl https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+
+ENV PATH ${PATH}:/usr/local/go/bin
+
+RUN go version
+
+ENV GOPATH /usr/local/go/src
+
+RUN go get github.com/braintree/manners
+
+RUN go get github.com/kelseyhightower/app/health
+
+RUN go get github.com/nand0p/app/handlers
+
+RUN go build -o /damnswank damnswank
+
+ENTRYPOINT ["/damnswank"]
---
+++ handlers/damnswank.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func DamnswankHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<html><head><title> ---=== !!!!DAMN SWANK!!!! ===--- </title>")
+    fmt.Fprintf(w, "<script type=text/javascript>var _gaq=_gaq||[];_gaq.push(['_setAccount','UA-37443405-1']);_gaq.push(['_trackPageview']);")
+    fmt.Fprintf(w, "(function(){var ga=document.createElement('script');ga.type='text/javascript';ga.async=true;")
+    fmt.Fprintf(w, "ga.src=('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';")
+    fmt.Fprintf(w, "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);")
+    fmt.Fprintf(w, "})();</script></head><body bgcolor=DD3322 text=pink vlink=blue>") 
+    fmt.Fprintf(w, "<center><font face=Helvetica><p><br><p><b><h1> &nbsp; </h1></b><p><br><p>")
+    fmt.Fprintf(w, "<a href=http://www.damnswank.com><img src=http://www.damnswank.com/damnswank.gif>")
+    fmt.Fprintf(w, "<p>&copy; 2001-2016 DAMNSWANK<br><p></a>9933CC</body></html>")
+}
